97af02b48919aa35f6ed86624dec3529080d3992
Gabi Nedelcheva <g.nedelcheva@vconnect.dk>
2021-11-29 16:43:40 +0200

    first task finished all point

--- libs/LocalStorage.js
+++ libs/LocalStorage.js
@@ -3,15 +3,44 @@ define([
 ], function ($) {
     'use strict';
 
+    $('#selectRole').change(function() {
+        localStorage.setItem('selectRole', this.value);
+    });
+    
 
     $("#save").on('click', function(){
-        localStorage.getItem('myCat')
-        localStorage.setItem('myCat', 'Tom');
+      
+        var person = { "firstName": 'lana', "lastName": "rey", "role": "manager"};//- default person
+    
+
+        localStorage.setItem('person', JSON.stringify(person)); //stringify object and store
+
+        //var retrievedPerson = JSON.parse(localStorage.getItem('person')); //retrieve the object
+        //var retrievedName = person[Object.keys(person)[0]];  //retrieve the key
+        //var retrievedName2 = person[Object.keys(person)[1]];  //retrieve the key
+        //var retrievedRole = person[Object.keys(person)[2]];  //retrieve the key
+
+        //console.log(retrievedPerson);
+        // console.log(retrievedName);
+        // console.log(retrievedName2);
+
+        $('input').each(function(){    
+            var id = $(this).attr('id');
+            var value = $(this).val();
+           localStorage.setItem(id, value);
+    
+        });  
+
+
     });
+
+
 
     $("#delete").on('click', function(){
-        localStorage.removeItem('myCat', 'Tom');
+        localStorage.removeItem('person');
+        localStorage.removeItem('selectRole');
+        localStorage.removeItem('firstName');
+        localStorage.removeItem('lastName');
     });
-
 
  });
--- libs/edit.js
+++ libs/edit.js
@@ -1,3 +1,7 @@
 require(['LocalStorage'] , function (LocalStorage) {
-
+         
+    document.getElementById("firstName").value = localStorage.getItem("firstName");
+    document.getElementById("lastName").value = localStorage.getItem("lastName");
+    document.getElementById("selectRole").value = localStorage.getItem("selectRole");
+ 
 });
